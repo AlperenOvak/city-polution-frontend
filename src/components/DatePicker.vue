d292cf6e250cfc12f480a9d4ee89ed845814cca5
Alperen Ovak <alperen.ovak@metu.edu.tr>
2025-08-22 13:52:26 +0300

    Add minDate and maxDate props to DatePicker component for date range constraints

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <MaDatePicker2 v-model="dateRange" label="Date Range" :range="true" />
+    <MaDatePicker2 
+      v-model="dateRange" 
+      label="Date Range" 
+      :range="true" 
+      :minDate="minDate"
+      :maxDate="maxDate"
+    />
   </div>
 </template>
 
 <script>
-import { ref, watch } from 'vue';
+import { ref, watch, computed } from 'vue';
 import { MaDatePicker2 } from '@mobileaction/action-kit';
 import { useSettingStore } from '../stores/settingStore';
 
@@ -18,12 +24,18 @@ export default {
     const settingStore = useSettingStore();
     const dateRange = ref(settingStore.dateRange);
 
+    // Date constraints: between 2020 and today
+    const minDate = computed(() => new Date('2020-11-27'));
+    const maxDate = computed(() => new Date());
+
     watch(dateRange, (newRange) => {
       settingStore.setDateRange(newRange);
     });
 
     return {
       dateRange,
+      minDate,
+      maxDate,
     };
   },
 };
